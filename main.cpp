ab91c15966531d6ab36a5a68953c13b42be0ec4b
snowkite <>
2012-05-27 15:23:53 +0900

    uncome args change bitset to 64

--- main.cpp
+++ main.cpp
@@ -11,34 +11,36 @@ using namespace std;
  * 1 - длина слова
  * 2 - количество слов
  **/
+
 int main(int argc, char** argv) {
     int n = 6;
     int k = 2;
 
-    //    if (argc == 3) {
-    //        n = atoi(argv[1]);
-    //        k = atoi(argv[2]);
-    //    }
-
+        if (argc == 3) {
+            n = atoi(argv[1]);
+            k = atoi(argv[2]);
+        }
+    
     unsigned int * a = new unsigned int[k];
     full_iterator<unsigned int> fi(a, n, k);
     greedy_algo<unsigned int> ga(a, n, k);
-    bitset < 10 > bs;
-    bitset < 10 > bs2;
+    bitset < 64 > bs;
+    bitset < 64 > bs2;
 
     while (fi.has_next()) {
         ++fi;
         bs = ga.invoke();
         for (int i = 0; i < k; i++) {
             bs2 = a[i];
-            if (bs[i])
-                cout << " *" << bs2.to_string().substr(10 - n, 10) << "* ";
-            else
-                cout << " [" << bs2.to_string().substr(10 - n, 10) << "] ";
+            if (bs[i]) {
+                cout << " *" << bs2.to_string().substr(64 - n, 64) << "* ";
+            } else {
+                cout << " [" << bs2.to_string().substr(64 - n, 64) << "] ";
+            }
         }
         cout << endl;
     }
 
-    return 0;
+    return 0;                
 }
 
